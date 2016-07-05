bd73955386cd7b92689644ae2a13f4a47e3d4a7d
mtaner <mervetaner88@gmail.com>
2016-07-05 11:38:10 +0100

    Server is set up

---
+++ app/server.js
@@ -0,0 +1,27 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer();
+var serverPort = 3000;
+
+server.on('request', simpleResponse);
+
+function simpleResponse(request, response) {
+	var url;
+
+	if(request.url === '/') {
+		url = './app/index.html'
+	} else {
+		url = './app/' + request.url;
+	};
+
+	var responseContent = fs.readFile(url, endResponse);
+
+	function endResponse(err, responseContent) {
+		if(err) console.error('There is an error', err)
+		response.end(responseContent, 'utf-8');
+	};
+};
+
+server.listen(serverPort, function() {
+	console.log('IT MIGHT BE WORKING?');
+});

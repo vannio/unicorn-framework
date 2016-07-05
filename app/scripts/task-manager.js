60bf80e53bd64604b76bbbf9d2c1fd0a03347674
Van Le <singularity@vannio.co.uk>
2016-07-05 14:25:47 +0100

    Added a function to mark tasks as complete

--- app/scripts/task-manager.js
+++ app/scripts/task-manager.js
@@ -1,11 +1,23 @@
 function TaskManager(){
   this.pendingTasks = [];
+  this.completedTasks = [];
 };
 
 TaskManager.prototype = {
   addTask: function(task){
-    // var task = task;
     this.pendingTasks.push(task);
+  },
+
+  markAsComplete: function(task){
+    if (this.pendingTasks.includes(task)){
+      var index = this.pendingTasks.indexOf(task);
+      this.completedTasks.push(task);
+      delete this.pendingTasks[index];
+
+      this.pendingTasks = this.pendingTasks.filter(function(element){
+        return element !== undefined;
+      });
+    }
   }
 };
 
--- tests/task-manager-test.js
+++ tests/task-manager-test.js
@@ -3,6 +3,7 @@ var TaskManager = require('../app/scripts/task-manager');
 
 describe('Task Manager', function(){
   var taskManager;
+  var testTask = { 1: 'This is a todo' };
 
   beforeEach(function(){
     taskManager = new TaskManager();
@@ -10,8 +11,17 @@ describe('Task Manager', function(){
 
   describe('#addTask', function(){
     it('should add a new task', function(){
-      taskManager.addTask({ 1: 'This is a todo' });
-      expect(taskManager.pendingTasks).to.include({ 1: 'This is a todo' });
+      taskManager.addTask(testTask);
+      expect(taskManager.pendingTasks).to.include(testTask);
+    });
+  });
+
+  describe('#markAsComplete', function(){
+    it('moves completed tasks from pending tasks', function(){
+      taskManager.addTask(testTask);
+      taskManager.markAsComplete(testTask);
+      expect(taskManager.pendingTasks).not.to.include(testTask);
+      expect(taskManager.completedTasks).to.include(testTask);
     });
   });
 });

e21b486f98eebc5ce57c8f90633eb04c738ba3f0
Jonathan <j_passe@live.fr>
2016-07-06 14:02:52 +0100

    list of tasks displaying on the index page

--- app/scripts/interface.js
+++ app/scripts/interface.js
@@ -6,5 +6,18 @@ $(document).ready(function() {
     event.preventDefault();
     var text = $('#task-content').val();
     taskManager.addTask(text);
+    // console.log(taskManager.pendingTasks.join("\n"));
+    $('#task-items').empty();
+    for(var i = 0; i < taskManager.pendingTasks.length; i++) {
+      $('#task-items').append('<li>' + taskManager.pendingTasks[i] + '</li>')
+
+    }
   });
+
 });
+
+// function displayItems(array) {
+//   for(var i = 0; i < array.length; i++) {
+//   '<li>' + array[i] + '</li>'
+//   }
+// }
